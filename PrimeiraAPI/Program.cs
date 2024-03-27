b4fab9f2e18e9c1fd297c87a2101a91a2df2d130
Giovanni Neves <giovanni_neves@tutanota.com>
2024-03-27 13:01:44 -0300

    Adicionando Context Adicionando pacotes para o postgresql

--- PrimeiraAPI/Program.cs
+++ PrimeiraAPI/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PrimeiraAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
